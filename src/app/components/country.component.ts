ead968c46a37b7b1bad64ae6a07c124985cc857a
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-21 16:13:11 +0200

    feat(component): add country component

---
+++ src/app/components/country.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input } from '@angular/core';
+import { Country } from '../../interfaces';
+
+@Component({
+  selector: 'app-country',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="w-full rounded grid grid-rows-2 gap-2 h-full">
+      <div class="w-full">
+        <img
+          class="object-cover rounded h-48 w-full"
+          [src]="country.flags.svg"
+          alt=""
+        />
+      </div>
+      <div class="m-4 flex flex-col justify-around">
+        <h2 class="text-2xl font-bold mb-4">{{ country.name.common }}</h2>
+
+        <div class="text-lg font-bold ml-2 -mt-2">
+          <p>
+            Population:
+            <span class="font-normal">{{ country.population }}</span>
+          </p>
+          <p>
+            Region: <span class="font-normal">{{ country.region }}</span>
+          </p>
+          <p>
+            Capital: <span class="font-normal"> {{ country.capital }}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  `,
+})
+export class CountryComponent {
+  @Input({ required: true }) country!: Country;
+}
