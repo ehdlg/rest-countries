46f33945f2d0e57a86bea802a24da1af30bf4908
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-21 10:28:14 +0200

    feat(service): add basic rest-countries service to fetch all countries

--- src/app/countries.service.ts
+++ src/app/countries.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { API_URL, COUNTRY_FIELDS } from '../constants';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountriesService {
-  constructor(private service: HttpClient) {}
+  constructor(private http: HttpClient) {}
+
+  get(filter = 'all?'): Observable<Country[]> {
+    return this.http.get<Country[]>(
+      API_URL + filter + COUNTRY_FIELDS.join(',')
+    );
+  }
 }
