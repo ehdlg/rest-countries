e97843cf3ccb0cd7bd1235ba55a0303ec0036325
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-21 10:29:04 +0200

    feat(constants): add COUNTRY_FIELDS constant to filter countries fields

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,14 @@
 export const API_URL = 'https://restcountries.com/v3.1/';
+
+export const COUNTRY_FIELDS = [
+  'name',
+  'flags',
+  'tld',
+  'currencies',
+  'capital',
+  'region',
+  'subregion',
+  'languages',
+  'population',
+  'flags',
+];
