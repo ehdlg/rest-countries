29bacd68ec80fa96f17dc73f536abe9edb2955a9
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-24 18:21:25 +0200

    feat(search): add search component, add filtery functionality, implement search component

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 import { CountriesComponent } from './countries.component';
 import { HeaderComponent } from './header.component';
 import { RouterOutlet } from '@angular/router';
 import { CountriesService } from '../services/countries.service';
 import { Observable } from 'rxjs';
-import { Country } from '../../interfaces';
+import { Country, AllRegions } from '../../interfaces';
+import { SearchComponent } from './search.component';
+import { REGION_FILTERS } from '../../constants';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CountriesComponent, HeaderComponent],
+  imports: [RouterOutlet, CountriesComponent, HeaderComponent, SearchComponent],
   template: `
     <app-header />
     <main class="max-w-[1900px] px-4 mt-20 mx-auto">
+      <app-search (regionEmitter)="updateRegion($event)" />
       <app-countries [data$]="data$" />
-      <router-outlet />
     </main>
+    <router-outlet />
   `,
 })
 export class AppComponent {
@@ -23,5 +26,11 @@ export class AppComponent {
 
   constructor(private service: CountriesService) {
     this.data$ = this.service.get();
+  }
+
+  public updateRegion(newRegion: AllRegions) {
+    const endpoint = REGION_FILTERS[newRegion];
+
+    this.data$ = this.service.get(endpoint);
   }
 }
---
+++ src/app/components/search.component.ts
@@ -0,0 +1,54 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgFor, TitleCasePipe } from '@angular/common';
+import { isValidRegion, Region, AllRegions } from '../../interfaces';
+import { REGION_FILTERS } from '../../constants';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [FormsModule, NgFor],
+  template: `
+    <div class="flex justify-between  w-full p-4 mb-12">
+      <form (submit)="onSubmit()">
+        <input
+          type="text"
+          placeholder="Search country"
+          name="country"
+          [(ngModel)]="country"
+          class="min-w-[200px] p-4 outline-none border-none rounded shadow-lg bg-white"
+        />
+        <button type="submit">submit</button>
+      </form>
+
+      <select
+        name=""
+        id=""
+        class="p-4 rounded bg-white shadow-lg hover:cursor-pointer"
+        (change)="updateRegion($event)"
+      >
+        <option *ngFor="let option of options" [value]="option">
+          {{ option }}
+        </option>
+      </select>
+    </div>
+  `,
+})
+export class SearchComponent {
+  public country = '';
+  public region = 'All';
+  protected options = Object.keys(REGION_FILTERS);
+  @Output() regionEmitter = new EventEmitter<AllRegions>();
+
+  onSubmit() {}
+
+  updateRegion(e: Event) {
+    if (!(e.target instanceof HTMLSelectElement)) return;
+
+    const newRegion = e.target.value;
+
+    if (!isValidRegion(newRegion)) return;
+
+    this.regionEmitter.emit(newRegion);
+  }
+}
