7268d92204d4465aa3f59ad48dd5c857f365dd4a
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-07-25 09:21:04 +0200

    feat(routes): add home route, detail route and wildcard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home.component';
+import { DetailsComponent } from './components/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: ':country',
+    component: DetailsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
