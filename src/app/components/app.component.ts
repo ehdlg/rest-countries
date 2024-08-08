b65968782dab85bf11741bbcd8f17f4393a59cc1
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-08 12:00:46 +0200

    feat(components): implement loader component in app component

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header.component';
+import { LoaderComponent } from './loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, LoaderComponent],
   template: `
     <app-header />
     <main class="max-w-[1900px] px-4 mt-20 mx-auto">
       <router-outlet />
+      <app-loader />
     </main>
   `,
 })
